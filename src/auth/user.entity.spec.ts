63fb53cbbda995860d9f7c7c8bc691a624511c3f
Gonçalves Antoine <antoine.goncalves-isc.france@ibm.com>
2021-05-12 11:49:50 +0200

    add test for app

---
+++ src/auth/user.entity.spec.ts
@@ -0,0 +1,31 @@
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+describe('User entity', () => {
+  let user: User;
+
+  beforeEach(() => {
+    user = new User();
+    user.password = 'testPassword14';
+    user.salt = 'testSalt';
+    bcrypt.hash = jest.fn();
+  });
+
+  describe('validatePassword', () => {
+    it('returns true as password is valid', async () => {
+      bcrypt.hash.mockReturnValue('testPassword14');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+      const result = await user.validatePassword('141010');
+      expect(bcrypt.hash).toHaveBeenCalledWith('141010', 'testSalt');
+      expect(result).toEqual(true);
+    });
+
+    it('returns false as password is invalid', async () => {
+      bcrypt.hash.mockReturnValue('wrongPassword');
+      expect(bcrypt.hash).not.toHaveBeenCalled();
+      const result = await user.validatePassword('wrongPassword');
+      expect(bcrypt.hash).toHaveBeenCalledWith('wrongPassword', 'testSalt');
+      expect(result).toEqual(false);
+    });
+  });
+});
